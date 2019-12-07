22055ff426a51db7bdf14930c6057fd00744fa13
durham-abric <durham.abric@mail.mcgill.ca>
2019-12-07 22:15:54 +0000

    Move output png

--- test.sh
+++ test.sh
@@ -5,9 +5,11 @@ args=${@:1:$arglen}
 last=${!#}
 
 rm -rf /wdata/*
+mkdir /wdata/output_png
 pushd /opt/app/src
 python create_spacenet_masks.py $args
 python train_eval.py resnet34_512_02_02.json
 python merge_preds.py
 python skeleton.py $last
+mv AOI*.png /wdata/output_png
 popd
